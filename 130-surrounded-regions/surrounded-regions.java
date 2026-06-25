345af81f099d88f2e283984c8e392329b7004aed
Fenil Gadhiya <gadhiyafenil090@gmail.com>
2026-06-25 23:48:37 +0530

    Time: 4 ms (17.17%) | Memory: 48 MB (10.74%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,13 +1,13 @@
 class Solution {
-    int dr[] = {-1, 0, 1, 0}; // t r b l
-    int dc[] = {0, 1, 0, -1};
+    int dr[] = { -1, 0, 1, 0 }; // t r b l
+    int dc[] = { 0, 1, 0, -1 };
 
-    static class Pair{
+    static class Pair {
         int r;
         int c;
         boolean v;
 
-        public Pair(int r, int c, boolean v){
+        public Pair(int r, int c, boolean v) {
             this.r = r;
             this.c = c;
             this.v = v;
@@ -18,12 +18,12 @@ class Solution {
         Queue<Pair> q = new LinkedList<>();
         int m = board.length;
         int n = board[0].length;
-        boolean[][]vis = new boolean[m][n];
+        boolean[][] vis = new boolean[m][n];
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(i == 0 || j == 0 || i == m-1 || j == n-1){
-                    if(board[i][j] == 'O'){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == m - 1 || j == n - 1) {
+                    if (board[i][j] == 'O') {
                         vis[i][j] = true;
                         q.add(new Pair(i, j, true));
                     }
@@ -31,23 +31,23 @@ class Solution {
             }
         }
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             Pair curr = q.remove();
-            for(int i=0; i<4; i++){
+            for (int i = 0; i < 4; i++) {
                 int nr = curr.r + dr[i];
                 int nc = curr.c + dc[i];
 
-                if(nr >= 0 && nc >= 0 && nr<m && nc<n && !vis[nr][nc] && board[nr][nc] == 'O'){
+                if (nr >= 0 && nc >= 0 && nr < m && nc < n && !vis[nr][nc] && board[nr][nc] == 'O') {
                     vis[nr][nc] = true;
                     q.add(new Pair(nr, nc, true));
-    
+
                 }
             }
         }
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(vis[i][j] == false){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (vis[i][j] == false) {
                     board[i][j] = 'X';
                 }
             }

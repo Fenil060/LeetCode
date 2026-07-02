3a7eb65be2ddacfa3499a1aba46e23d60c08e191
Fenil Gadhiya <gadhiyafenil090@gmail.com>
2026-07-02 16:48:42 +0530

    Time: 4 ms (82.22%) | Memory: 44.3 MB (82.27%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+        }
+
+        for (int j = 0; j < t.length(); j++) {
+            arr[t.charAt(j) - 'a']--;
+        }
+
+        for (int a : arr) {
+            if (a != 0)
+                return false;
+        }
+        return true;
+    }
+}

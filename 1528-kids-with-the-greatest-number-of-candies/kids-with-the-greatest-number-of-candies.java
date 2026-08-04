b28b0f811edb3775a5966fdf46cc7fea638c4459
Fenil Gadhiya <gadhiyafenil090@gmail.com>
2026-08-04 11:21:48 +0530

    Time: 1 ms (96.16%) | Memory: 44.1 MB (24.82%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        for (int c : candies) {
+            max = Math.max(max, c);
+        }
+
+        List<Boolean> ans = new ArrayList<>();
+        for (int c : candies) {
+            if (c + extraCandies >= max) {
+                ans.add(true);
+            } else {
+                ans.add(false);
+            }
+        }
+        return ans;
+    }
+}

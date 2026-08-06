1a56609b2e781410833afd72a44141a9cb435f01
Fenil Gadhiya <gadhiyafenil090@gmail.com>
2026-08-06 21:38:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (94.57%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        return helper(n, dp);
+    }
+
+    public int helper(int n, int[]dp){
+    
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
